f5d38c539d0f761b326df6de61ec765792b0310c
cdrJsus <f_cederquist@hotmail.com>
2020-03-11 10:46:46 +0100

    tja

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,7 +13,7 @@ export default function HomeScreen({ navigation }) {
           <View style={styles.textContainer}>
             <Text style={styles.text}>Välkommen till{"\n"}Örebro Universitets{"\n"}Studentapp</Text>
             <Text>TJEJsijeiajsiejasijeias</Text>
-            <Text>hejsanSvejsan</Text>
+            <Text>hejsanSvejsanDAvey</Text>
           </View>
           <View style={styles.buttonContainer}>
             <View style={styles.facebookLogInButton}>
