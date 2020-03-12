a9a9293fef3fcb2d959e2dbbef762d5cf3723d7b
Kristian Nilsson <Nilsson_Kristian@outlook.com>
2020-03-12 11:03:08 +0100

    fixat koden lite

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -4,13 +4,13 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function RegisterScreen({ route, navigation }) {
-    const { itemId } = route.params;
-    return (
-        <LinearGradient style={styles.linearGradient} colors={['#0E6888', '#4E83A2', '#57C9A9']}>
+  const { itemId } = route.params;
+  return (
+    <LinearGradient style={styles.linearGradient} colors={['#0E6888', '#4E83A2', '#57C9A9']}>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
         <Text>itemId: {JSON.stringify(itemId)}</Text>
-  
+
         {/* Renderar sidan igen även om vi är på denna just nu */}
         <Button
           title="Go to Details... again"
@@ -26,12 +26,12 @@ export default function RegisterScreen({ route, navigation }) {
         <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
         <Button title="Go back" onPress={() => navigation.goBack()} />
       </View>
-      </LinearGradient>
-    );
-  }
+    </LinearGradient>
+  );
+}
 
-  const styles = StyleSheet.create({
-    linearGradient: {
-        height: '100%'
-      }
-  })
+const styles = StyleSheet.create({
+  linearGradient: {
+    height: '100%'
+  }
+})
