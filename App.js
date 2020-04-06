28e30114d80d839f89ba7deebf7e13476385ca1a
Kristian Nilsson <Nilsson_Kristian@outlook.com>
2020-04-06 13:03:33 +0200

    app.js kommentar

--- App.js
+++ App.js
@@ -2,7 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react'; 
 import Navigator from './routes/drawer.js';
 
+//-----------------------------------------------------------------------------------------------------------------------------------
+// SAKER ATT FIXA I APPEN
+//-----------------------------------------------------------------------------------------------------------------------------------
 
+// När man är på settingsscreen och och trycker på hamburgermenyn och sedan homescreen 
+// kommer man ett steg tillbaka till registerscreen och inte homescreen 
 
 export default function App() {
   return (
