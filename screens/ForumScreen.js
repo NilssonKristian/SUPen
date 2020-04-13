5c6387ddb32545a6f1b8ffff40a2309280ebc44a
Kristian Nilsson <Nilsson_Kristian@outlook.com>
2020-04-13 13:23:28 +0200

    adderat footer på alla sidor

--- screens/ForumScreen.js
+++ screens/ForumScreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
+import Footer from '../components/Footer.js';
+
 export class ForumScreen extends Component {
     render() {
         return (
             <div>
                 <text>Forum Screen</text>
                 <text>Här ska man se ett forum som man klickat på.</text>
+                <Footer />
             </div>
         )
     }
